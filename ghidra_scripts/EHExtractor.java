e84ee4b510b394a3d571aad652503a637d8d712f
Remco <rslijkhu@gmail.com>
2024-04-12 14:49:33 +0200

    Code specific to the 'ehhandler function' thet MSVC uses moved to its own class.

--- ghidra_scripts/EHExtractor.java
+++ ghidra_scripts/EHExtractor.java
@@ -41,6 +41,7 @@ import java.util.Date;
 import instructionpattrns.*;
 import loggingbridge.GhidraScriptHandler;
 import msvc.exceptions.*;
+import msvc.exceptions.code.EHHandler;
 
 public class EHExtractor extends GhidraScript {  
 	final Level LOG_LEVEL = Level.ALL;
@@ -48,8 +49,7 @@ public class EHExtractor extends GhidraScript {
 	Logger logger = null;
 	FileHandler fh = null;
 	
-	Function cxxFrameHandler3 = null;
-	Function securityCheckCookie = null;
+	EHHandler ehHandler = null;
 	
     public void run() throws Exception {
     	// Set up a proper logger first. Exit when there are problems doing so.
@@ -78,28 +78,10 @@ public class EHExtractor extends GhidraScript {
     		logger.log(Level.INFO, "Program spans addresses "+minAddr+"-"+maxAddr);
     	
 
-    		// If there are exceptions, we expect an exception handler. The main one for x86
-    		// is CxxFrameHandler3. we should look for this; note: could have thunks.
-    		//
-    		// Find the function vcruntime*CxxFrameHandler3.
-    		logger.log(Level.FINE, "Determining the address of (thunk) function *CxxFrameHandler3.");
-    		cxxFrameHandler3 = FunctionUtils.findFunction(currentProgram, "CxxFrameHandler3", "vcruntime", true);
-    		if (cxxFrameHandler3 == null) {
-    			logger.log(Level.INFO, "Main exception handler function not found!");
+    		// Create an EHHandler instance suitable for the current program.
+    		ehHandler = new EHHandler(currentProgram);
+    		if (!ehHandler.isAllOk()) {
     			return;
-    		}
-
-    		// If security cookies have been used, there should be a function called "security_check_cookie".
-    		// We don't know if they have been used, so we don't know if we'll find this function.
-    		// Also, when this function is present, it is sometimes present more than once.
-    		// In any case, whether we find 'one' function with this name or not, we'll carry on.    		
-    		logger.log(Level.FINE, "Looking up a security_check_cookie function.");
-    		securityCheckCookie = FunctionUtils.findFunction(currentProgram, "security_check_cookie", null, true);
-    		if (securityCheckCookie != null) {
-    			logger.log(Level.FINE, "Found a security_check_cookie function: " + securityCheckCookie.getName() + " @" + securityCheckCookie.getEntryPoint());
-    		}
-    		else {
-    			logger.log(Level.FINE, "No clear security_check_cookie function found.");			
     		}
 
 
@@ -183,103 +165,27 @@ public class EHExtractor extends GhidraScript {
 		// Do we at least see registering of an ehFuncInfo?  Is cookie-checking code included or not?
 		// Note: registering involves putting an address in EAX and JMPing to CxxFrameHandler3,
 		//   but this JMP may not follow the MOV EAX immediately; there may be an extra JMP in between,
-		//   so a JMP to the JMP CxxFrameHandler3 (stubbing).
+		//   so a JMP to the JMP CxxFrameHandler3 (thunking).
 		checkExceptionHandlerInstructions(listing, ehSetupAddress);
     }
 
     
     private void checkExceptionHandlerInstructions(Listing listing, Address ehSetupAddress) {
 
-		Address ehFunctionInfoAddress = extractEHFunctionInfoAddress(listing, ehSetupAddress);
-		if (ehFunctionInfoAddress == null)
+    	Address ehFuncInfoAddress = ehHandler.extractFuncInfoAddress(listing, ehSetupAddress);
+		if (ehFuncInfoAddress == null)
 			return;
 		
 		MSVCEHInfo msvcEHInfo = null;
 		try {
 			logger.log(Level.FINE, "About to process the EH data structures.");
-			//msvcEHInfo = new MSVCEHInfo(currentProgram, ehFunctionInfoAddress);
-			msvcEHInfo = MSVCEHInfoFactory.getMSVCEHInfo(currentProgram, ehFunctionInfoAddress);
+			msvcEHInfo = MSVCEHInfoFactory.getMSVCEHInfo(currentProgram, ehFuncInfoAddress);
 			msvcEHInfo.analyze();
 		}
 		catch (InvalidDataTypeException e) { 
 			logger.log(Level.SEVERE, "OH NOES! "+ e.getMessage());
 		}
 		
-	}
-
-	private Address extractEHFunctionInfoAddress(Listing listing, Address startAddress) {
-		// Check for cookie-checking code.
-		var matchResult = lookForCookieCheckingCode(listing, startAddress);
-		
-		// Note: If no cookie-checking code, getNextAddress() will return the original start address.
-		// If there is cookie-checking code, getNextAddress() will return the first address after this code,
-		// however many times the cookie was checked.
-		InstructionIterator instIt = listing.getInstructions(matchResult.getNextAddress(), true);
-
-		// Exception handler function info registration.
-		List<InstructionPattern> regInstructions = Arrays.asList(
-				new ScalarInstructionPattern("MOV", "EAX", Scalar.class),
-				new AddressInstructionPattern("JMP", cxxFrameHandler3, true)
-		);
-		
-		logger.log(Level.FINE, "Looking for matching EH handler registration instructions.");
-		if (!InstructionPatterns.match(regInstructions, instIt, true).isMatched()) {
-			logger.log(Level.INFO, "EH handler registration instructions not found!");
-			return null;
-		}
-		logger.log(Level.INFO, "EH handler registration instructions found!");
-
-		Scalar scalar = ((ScalarInstructionPattern)regInstructions.get(0)).getActualScalar();
-		var ehFuncInfoAddress = makeAddress(scalar.getUnsignedValue());
-		logger.log(Level.INFO, "Determined ehFuncInfoAddress: " + ehFuncInfoAddress);
-		return ehFuncInfoAddress;
-	}
-
-	private MatchResult lookForCookieCheckingCode(Listing listing, Address startAddress) {
-
-		// Generic/flexible base cookie-checking code instructions.
-		// If there is cookie-checking, the following code will be there at the start.
-		List<InstructionPattern> baseCookieCheckInstructions = Arrays.asList(
-				new AddressInstructionPattern("MOV", "EDX", "ESP", Scalar.class),
-				new AddressInstructionPattern("LEA", "EAX", "EDX", (long)0xc),			
-				new AddressInstructionPattern("MOV", "ECX", "EDX", Scalar.class), 
-				new RegisterInstructionPattern("XOR", "ECX", "EAX"),
-				new AddressInstructionPattern("CALL", securityCheckCookie, true)
-		);
-
-		// Generic/flexible additional cookie-checking code instructions.
-		// If there is mode cookie-checking going on, the following code will
-		// be there, possibly multiple times.
-		List<InstructionPattern> additionalCookieCheckInstructions = Arrays.asList(
-				new AddressInstructionPattern("MOV", "ECX", "EDX", Scalar.class), 
-				new RegisterInstructionPattern("XOR", "ECX", "EAX"),
-				new AddressInstructionPattern("CALL", securityCheckCookie, true)
-		);
-
-		// Look for base cookie-checking code.
-		logger.log(Level.FINE, "Looking for base security cookie-checking code.");
-		InstructionIterator instIt = listing.getInstructions(startAddress, true);
-		MatchResult matchResult = InstructionPatterns.match(baseCookieCheckInstructions, instIt, true);
-		if (!matchResult.isMatched()) {
-			logger.log(Level.FINE, "Base cookie checking instructions not found!");
-			return new MatchResult(false, startAddress);
-		}
-		logger.log(Level.FINE, "Base cookie checking instructions found!");
-		startAddress = matchResult.getNextAddress();
-
-		// There IS base cookie-checking code. Is it followed by additional cookie-checking code?
-		// Note that we don't know how many instances of additional cookie-checking code there can be.
-		logger.log(Level.FINE, "Looking for additional security cookie-checking code.");
-		while (matchResult.isMatched()) {
-			instIt = listing.getInstructions(startAddress, true);
-			matchResult = InstructionPatterns.match(additionalCookieCheckInstructions, instIt, true);
-			if (matchResult.isMatched()) {
-				logger.log(Level.FINE, "Additional cookie checking instructions found.");
-				startAddress = matchResult.getNextAddress();
-			}
-		}
-		logger.log(Level.FINE, "Additional cookie checking instructions not found.");
-		return new MatchResult(true, startAddress);		
 	}
 
 	public Address makeAddress(Scalar scalar) {
---
+++ src/main/java/msvc/exceptions/code/EHHandler.java
@@ -0,0 +1,157 @@
+package msvc.exceptions.code;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import ehextractor.FunctionUtils;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressFactory;
+import ghidra.program.model.address.AddressSpace;
+import ghidra.program.model.listing.Function;
+import ghidra.program.model.listing.InstructionIterator;
+import ghidra.program.model.listing.Listing;
+import ghidra.program.model.listing.Program;
+import ghidra.program.model.scalar.Scalar;
+import instructionpattrns.AddressInstructionPattern;
+import instructionpattrns.InstructionPattern;
+import instructionpattrns.InstructionPatterns;
+import instructionpattrns.MatchResult;
+import instructionpattrns.RegisterInstructionPattern;
+import instructionpattrns.ScalarInstructionPattern;
+
+public class EHHandler {
+
+	private Program program = null; // I don't like this at all, but it is needed for makeAddress.
+	private Function cxxFrameHandler3 = null;
+	private Function securityCheckCookie = null;
+	private boolean allOk;
+
+	private Logger logger = null;
+
+	public EHHandler(Program program) {
+		logger = Logger.getLogger("EHExtractor");
+		this.program = program;
+		
+		allOk = initialize();		
+	}
+	
+	public boolean isAllOk() {
+		return allOk;
+	}
+
+	private boolean initialize() {
+		// If there are exceptions, we expect an exception handler. The main one for x86
+		// is CxxFrameHandler3. We should look for this; note: could have thunks.
+		//
+		// Find the function vcruntime*CxxFrameHandler3.
+		logger.log(Level.FINE, "Determining the address of (thunk) function *CxxFrameHandler3.");
+		cxxFrameHandler3 = FunctionUtils.findFunction(program, "CxxFrameHandler3", "vcruntime", true);
+		if (cxxFrameHandler3 == null) {
+			logger.log(Level.INFO, "Main exception handler function not found!");
+			return false;
+		}
+
+		// If security cookies have been used, there should be a function called "security_check_cookie".
+		// We don't know if they have been used, so we don't know if we'll find this function.
+		// Also, when this function is present, it is sometimes present more than once.
+		// In any case, whether we find 'one' function with this name or not, we'll carry on.    		
+		logger.log(Level.FINE, "Looking up a security_check_cookie function.");
+		securityCheckCookie = FunctionUtils.findFunction(program, "security_check_cookie", null, true);
+		if (securityCheckCookie != null) {
+			logger.log(Level.FINE, "Found a security_check_cookie function: " + securityCheckCookie.getName() + " @" + securityCheckCookie.getEntryPoint());
+		}
+		else {
+			logger.log(Level.FINE, "No clear security_check_cookie function found.");			
+		}
+
+		return true;		
+	}
+	
+	
+	public Address extractFuncInfoAddress(Listing listing, Address ehSetupAddress) {
+		Address startAddress = ehSetupAddress;
+
+		// Check for cookie-checking code.
+		var matchResult = lookForCookieCheckingCode(listing, startAddress, securityCheckCookie);
+
+		// Note: If no cookie-checking code, getNextAddress() will return the original start address.
+		// If there is cookie-checking code, getNextAddress() will return the first address after this code,
+		// however many times the cookie was checked.
+		InstructionIterator instIt = listing.getInstructions(matchResult.getNextAddress(), true);
+
+		// Exception handler function info registration.
+		List<InstructionPattern> regInstructions = Arrays.asList(
+				new ScalarInstructionPattern("MOV", "EAX", Scalar.class),
+				new AddressInstructionPattern("JMP", cxxFrameHandler3, true)
+		);
+
+		logger.log(Level.FINE, "Looking for matching EH handler registration instructions.");
+		if (!InstructionPatterns.match(regInstructions, instIt, true).isMatched()) {
+			logger.log(Level.INFO, "EH handler registration instructions not found!");
+			return null;
+		}
+		logger.log(Level.INFO, "EH handler registration instructions found!");
+
+		Scalar scalar = ((ScalarInstructionPattern)regInstructions.get(0)).getActualScalar();
+		var ehFuncInfoAddress = makeAddress(scalar.getUnsignedValue());
+		logger.log(Level.INFO, "Determined ehFuncInfoAddress: " + ehFuncInfoAddress);
+		return ehFuncInfoAddress;
+	}
+	
+	private MatchResult lookForCookieCheckingCode(Listing listing, Address startAddress, Function securityCheckCookie) {
+		// Generic/flexible base cookie-checking code instructions.
+		// If there is cookie-checking, the following code will be there at the start.
+		List<InstructionPattern> baseCookieCheckInstructions = Arrays.asList(
+				new AddressInstructionPattern("MOV", "EDX", "ESP", Scalar.class),
+				new AddressInstructionPattern("LEA", "EAX", "EDX", (long)0xc),			
+				new AddressInstructionPattern("MOV", "ECX", "EDX", Scalar.class), 
+				new RegisterInstructionPattern("XOR", "ECX", "EAX"),
+				new AddressInstructionPattern("CALL", securityCheckCookie, true)
+		);
+
+		// Generic/flexible additional cookie-checking code instructions.
+		// If there is mode cookie-checking going on, the following code will
+		// be there, possibly multiple times.
+		List<InstructionPattern> additionalCookieCheckInstructions = Arrays.asList(
+				new AddressInstructionPattern("MOV", "ECX", "EDX", Scalar.class), 
+				new RegisterInstructionPattern("XOR", "ECX", "EAX"),
+				new AddressInstructionPattern("CALL", securityCheckCookie, true)
+		);
+
+		// Look for base cookie-checking code.
+		logger.log(Level.FINE, "Looking for base security cookie-checking code.");
+		InstructionIterator instIt = listing.getInstructions(startAddress, true);
+		MatchResult matchResult = InstructionPatterns.match(baseCookieCheckInstructions, instIt, true);
+		if (!matchResult.isMatched()) {
+			logger.log(Level.FINE, "Base cookie checking instructions not found!");
+			return new MatchResult(false, startAddress);
+		}
+		logger.log(Level.FINE, "Base cookie checking instructions found!");
+		startAddress = matchResult.getNextAddress();
+
+		// There IS base cookie-checking code. Is it followed by additional cookie-checking code?
+		// Note that we don't know how many instances of additional cookie-checking code there can be.
+		logger.log(Level.FINE, "Looking for additional security cookie-checking code.");
+		while (matchResult.isMatched()) {
+			instIt = listing.getInstructions(startAddress, true);
+			matchResult = InstructionPatterns.match(additionalCookieCheckInstructions, instIt, true);
+			if (matchResult.isMatched()) {
+				logger.log(Level.FINE, "Additional cookie checking instructions found.");
+				startAddress = matchResult.getNextAddress();
+			}
+		}
+		logger.log(Level.FINE, "Additional cookie checking instructions not found.");
+		return new MatchResult(true, startAddress);		
+	}
+
+	// TODO: Get rif of this function here.
+    private Address makeAddress(long address) {
+		AddressFactory addressFactory = program.getAddressFactory();
+		AddressSpace defaultAddressSpace = addressFactory.getDefaultAddressSpace();
+		Address newAddress = defaultAddressSpace.getAddress(address);
+		return newAddress;
+    }
+
+}
