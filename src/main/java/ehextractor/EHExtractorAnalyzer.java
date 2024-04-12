689b0af230736ed58a08e6cc6bba9519e10dd930
Remco <rslijkhu@gmail.com>
2024-04-12 16:40:22 +0200

    First version of the Analyzer version of EHExtractor (with many things still hardwired).

--- src/main/java/ehextractor/EHExtractorAnalyzer.java
+++ src/main/java/ehextractor/EHExtractorAnalyzer.java
@@ -15,6 +15,7 @@
  */
 package ehextractor;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,16 +25,29 @@ import ghidra.app.services.AbstractAnalyzer;
 import ghidra.app.services.AnalyzerType;
 import ghidra.app.util.importer.MessageLog;
 import ghidra.framework.options.Options;
+import ghidra.program.model.address.Address;
 import ghidra.program.model.address.AddressSetView;
+import ghidra.program.model.data.InvalidDataTypeException;
+import ghidra.program.model.listing.Function;
 import ghidra.program.model.listing.Program;
 import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
+import msvc.exceptions.MSVCEHInfo;
+import msvc.exceptions.MSVCEHInfoFactory;
+import msvc.exceptions.code.EHHandler;
+import msvc.exceptions.code.Prologue;
 
 /**
  * TODO: Provide class-level documentation that describes what this analyzer does.
  */
 public class EHExtractorAnalyzer extends AbstractAnalyzer {
 
+	Program program = null;
+	Prologue prologue = null;
+	EHHandler ehHandler = null;
+
+	Logger logger = null;
+	
 	public EHExtractorAnalyzer() {
 		// TODO: Correct AnalyzerType? 
 		super("EHExtractor", "Extracts x86 MSVC exception handling construct information.", AnalyzerType.FUNCTION_ANALYZER);
@@ -74,7 +88,10 @@ public class EHExtractorAnalyzer extends AbstractAnalyzer {
 		// TODO: Perform analysis when things get added to the 'program'.  Return true if the
 		// analysis succeeded.
 
+		this.program= program;
+
 		Logging logging = null;
+
 
 		try {
 			// Set up logging.
@@ -84,8 +101,32 @@ public class EHExtractorAnalyzer extends AbstractAnalyzer {
 	    		return false;
 	    	}
 
-			var logger = Logger.getLogger("EHExtractor");
-			logger.log(Level.INFO, "This is a test message.");
+			logger = Logger.getLogger("EHExtractor");
+
+			// Program name and address space information.
+    		Address minAddr = program.getMinAddress();
+    		Address maxAddr = program.getMaxAddress();
+    		logger.log(Level.INFO, "Program file: "+program.getExecutablePath());
+    		logger.log(Level.INFO, "Program spans addresses "+minAddr+"-"+maxAddr);
+
+    		// Create a Prologue instance.
+    		prologue = new Prologue(program);
+
+    		// Create an EHHandler instance suitable for the current program.
+    		ehHandler = new EHHandler(program);
+    		if (!ehHandler.isAllOk()) {
+    			return false;
+    		}
+
+    		logger.log(Level.FINE, "Now going to look at some functions.");
+    	
+    		List<Function> allFuncs = FunctionUtils.getInternalFunctions(program);
+
+    		for (var func : allFuncs) {
+    			logger.log(Level.INFO, "");
+    			showFunctionInfo(func);
+    		}
+    		
 		}
 		finally {
 			logging.close();
@@ -93,4 +134,47 @@ public class EHExtractorAnalyzer extends AbstractAnalyzer {
 		
 		return false;
 	}
+	
+    public void showFunctionInfo(Function func) {
+    	// Show the name and memory location range of the function.
+    	logger.log(Level.INFO, "Looking at: "+func.getName());
+        long addrStart = func.getBody().getMinAddress().getOffset();
+        long addrEnd = func.getBody().getMaxAddress().getOffset();
+        logger.log(Level.INFO, String.format("Memory range: %08x-%08x", addrStart, addrEnd));
+
+        logger.log(Level.FINE, "Let's start with the instructions:");
+
+        // Look at the start of the function; does it have the expected format?
+        // If so, get the address of 'ehhandler' / the 'EH setup code'.
+		Address ehSetupAddress = prologue.extractEHSetupAddress(func);
+		if (ehSetupAddress == null)
+			return;
+
+		logger.log(Level.FINE, "Going to look at the supposed EH setup code at location "+ehSetupAddress.toString(true));
+
+		// We have a location! There should be a certain set of instructions there.
+		// Should evaluate the instructions: do they match with expectations?
+		// Do we at least see registering of an ehFuncInfo?  Is cookie-checking code included or not?
+		// Note: registering involves putting an address in EAX and JMPing to CxxFrameHandler3,
+		//   but this JMP may not follow the MOV EAX immediately; there may be an extra JMP in between,
+		//   so a JMP to the JMP CxxFrameHandler3 (thunking).
+
+    	// If our expectations are met, we can get FuncInfo's memory location.
+    	Address ehFuncInfoAddress = ehHandler.extractFuncInfoAddress(ehSetupAddress);
+		if (ehFuncInfoAddress == null)
+			return;
+		
+		// With the location of the FuncInfo data structure found, let's try
+		// to parse it and the connected data structures.
+		try {
+			logger.log(Level.FINE, "About to process the EH data structures.");
+			MSVCEHInfo msvcEHInfo = MSVCEHInfoFactory.getMSVCEHInfo(program, ehFuncInfoAddress);
+			msvcEHInfo.analyze();
+		}
+		catch (InvalidDataTypeException e) { 
+			logger.log(Level.SEVERE, "OH NOES! "+ e.getMessage());
+		}
+		
+	}
+
 }
