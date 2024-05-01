5e2af7359b2619df1e64ac91b06927137f63a4d2
Remco <rslijkhu@gmail.com>
2024-05-01 09:49:17 +0200

    Analyzer starting, working, stopping is now shown in the project window (and in the application log). The output log location is included in these messages.

--- src/main/java/ehextractor/EHExtractorAnalyzer.java
+++ src/main/java/ehextractor/EHExtractorAnalyzer.java
@@ -29,6 +29,7 @@ import ghidra.framework.options.OptionType;
 import ghidra.framework.options.Options;
 import ghidra.program.model.address.AddressSetView;
 import ghidra.program.model.listing.Program;
+import ghidra.util.Msg;
 import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
 
@@ -45,13 +46,15 @@ public class EHExtractorAnalyzer extends AbstractAnalyzer {
 	    FINER, FINE, INFO;
 	}
 
+	private static final String ANALYZER_NAME = "EHExtractor";
+
 	private static final String OPTION_LOG_FILE_PATH = "Log file path";
 	private static final String OPTION_LOG_LEVEL = "Minimum log level";
-	private static final String OPTION_SHOW_LOG_LEVEL = "Show log level";
+	private static final String OPTION_SHOW_LOG_LEVEL = "Prefix log level";
 
 	private static final LogLevelEnum OPTION_LOG_LEVEL_DEFAULT = LogLevelEnum.INFO;
 	private static final File OPTION_LOG_FILE_PATH_DEFAULT = Paths.get(System.getProperty("user.home"), "Documents", "ehextractor.log").toFile();
-	private static final boolean OPTION_SHOW_LOG_LEVEL_DEFAULT = false;
+	private static final boolean OPTION_PREFIX_LOG_LEVEL_DEFAULT = false;
 
 	private String logFilePath = null;
 	private Level logLevel = Level.ALL;
@@ -88,7 +91,7 @@ public class EHExtractorAnalyzer extends AbstractAnalyzer {
 	public void registerOptions(Options options, Program program) {
 		options.registerOption(OPTION_LOG_LEVEL, OPTION_LOG_LEVEL_DEFAULT, null, "Minimum log level.");
 		options.registerOption(OPTION_LOG_FILE_PATH, OptionType.FILE_TYPE, OPTION_LOG_FILE_PATH_DEFAULT, null, "Path to the log file.");
-		options.registerOption(OPTION_SHOW_LOG_LEVEL, OPTION_SHOW_LOG_LEVEL_DEFAULT, null, "Show log level.");
+		options.registerOption(OPTION_SHOW_LOG_LEVEL, OPTION_PREFIX_LOG_LEVEL_DEFAULT, null, "Prefix output lines with their log level.");
 	}
 
 	@Override
@@ -99,7 +102,7 @@ public class EHExtractorAnalyzer extends AbstractAnalyzer {
 		LogLevelEnum value = options.getEnum(OPTION_LOG_LEVEL, null);
 		logLevel = convertLogLevel(value);
 		
-		showLogLevel = options.getBoolean(OPTION_SHOW_LOG_LEVEL, OPTION_SHOW_LOG_LEVEL_DEFAULT);
+		showLogLevel = options.getBoolean(OPTION_SHOW_LOG_LEVEL, OPTION_PREFIX_LOG_LEVEL_DEFAULT);
 	}
 
 	private Level convertLogLevel(LogLevelEnum logLevel) {
@@ -118,6 +121,8 @@ public class EHExtractorAnalyzer extends AbstractAnalyzer {
 	@Override
 	public boolean added(Program program, AddressSetView set, TaskMonitor monitor, MessageLog log) throws CancelledException {
 		
+		Msg.info(this, String.format("%s starting work on %s. Output log location is %s.", ANALYZER_NAME, program.getName(), logFilePath.toString()));
+		
 		// Start looking for (and logging) MSVC EH information in the given program. 
 		Logging logging = null;
 		try {
@@ -125,17 +130,20 @@ public class EHExtractorAnalyzer extends AbstractAnalyzer {
 			logging = new Logging(logFilePath, logLevel, showLogLevel);
 	    	if (logging == null || !logging.isSetupSuccess()) {
 	    		Log.error("Logger setup not successful. Unable to continue.");
+        		Msg.error(this, String.format("%s encountered a problem during logger setup. Check the output log at %s for more information.", ANALYZER_NAME, logFilePath.toString()));
 	    		return false;
 	    	}
 
     		// Log global information about the file and set up some required internal objects.
     		var ehExtractor = new EHExtractor(program);
     		if (!ehExtractor.isAllOk()) {
+        		Msg.error(this, String.format("%s encountered a problem during setup. Check the output log at %s for more information.", ANALYZER_NAME, logFilePath.toString()));
     			return false;
     		}
     		// Everything ready to go. Let's look for EH constructs!
+    		Msg.info(this, ANALYZER_NAME + " is looking for EH constructs.");
     		ehExtractor.showFunctionInfos();
-    		
+    		Msg.info(this, String.format("%s finished. Output log location is %s.", ANALYZER_NAME, logFilePath.toString()));
 		}
 		finally {
     		// Close the file used for logging.
